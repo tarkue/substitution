82634fd2b8b78b3b30c8a63d7c30d0ceb5057ede
tarkue <mrfrostlikchannel@gmail.com>
2025-02-14 22:20:16 +0500

    add setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup 
+
+setup(
+    name='substitution',
+    version='1.0.0',
+    description='Implementing Substitutions with multiplication, exponentiation, comparison, length output, cycle search and power search',
+    author='tarkue',
+    author_email='mrlimax@icloud.com',
+)
